a7cd7a35c0a9b0b7a7e550b20c5e28f59d312659
JonEast87 <j.d.east@icloud.com>
2023-07-05 21:18:34 -0700

    nearly finished with the second chapter

---
+++ Chap2/abstractClass.ts
@@ -0,0 +1,42 @@
+namespace AbstractNamespace {
+    abstract class Vehicle {
+        constructor(protected wheelCount: number) {}
+
+        abstract updateWheelCount(newWheelCount: number): void;
+
+        showNumberOfWheels() {
+            console.log(`moved ${this.wheelCount} miles`);
+        }
+    }
+
+    class MotorCycle extends Vehicle {
+        constructor() {
+            super(2);
+        }
+
+        updateWheelCount(newWheelCount: number) {
+            this.wheelCount = newWheelCount;
+            console.log(`Motorcycle this ${this.wheelCount}`);
+        }
+    }
+
+    class Automobile extends Vehicle {
+        constructor() {
+            super(4);
+        }
+
+        updateWheelCount(newWheelCount: number) {
+            this.wheelCount = newWheelCount;
+            console.log(`Automobile has ${this.wheelCount}`);
+        }
+
+        showNumberOfWheels() {
+            console.log(`moved ${this.wheelCount} miles`);
+        }
+    }
+
+    const motorCycle = new MotorCycle();
+    motorCycle.updateWheelCount(1);
+    const autoMobile = new Automobile();
+    autoMobile.updateWheelCount(3);
+}
