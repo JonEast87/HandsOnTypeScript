13cce9fa3f75238ce40ea13df726871d367332f2
JonEast87 <j.d.east@icloud.com>
2023-07-05 22:00:19 -0700

    working on chapter 3

---
+++ Chap3/functionBody.ts
@@ -0,0 +1,20 @@
+// Working on scoping, commented out to prevent errors
+// if (true) {
+//     var val = 1;
+// }
+//
+// function go() {
+//     var val2 = 2;
+// }
+//
+// console.log(val1);
+// console.log(val2);
+
+// namespace constants {
+//     const val1 = 1;
+//     val1 = 2;
+//
+//     const val2 = [];
+//
+//     val2.push('hello');
+// }
