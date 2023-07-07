e8c702e53688d22008591dd41e3bea50769c1c92
JonEast87 <j.d.east@icloud.com>
2023-07-06 21:40:12 -0700

    finished chapter 3

---
+++ Chap3/promise.js
@@ -0,0 +1,12 @@
+const myPromise = new Promise((resolve, reject) => {
+   setTimeout(() => {
+      // resolve ('I completed successfully.');
+      reject('I failed');
+   }, 500);
+});
+
+myPromise.then(done => {
+    console.log(done);
+}).catch(err => {
+        console.log(err);
+});
---
+++ Chap3/setTimer.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+    console.log('I waited and am done now.');
+}, 3000);
+
+console.log('Did I finish yet?');
