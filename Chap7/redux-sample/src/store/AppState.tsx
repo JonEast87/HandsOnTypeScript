388cf9db8d8ef4ce0a8666481a813f1bf4645145
JonEast87 <j.d.east@icloud.com>
2023-07-13 18:20:59 -0700

    wrapping up work on json api

--- Chap7/redux-sample/src/store/AppState.tsx
+++ Chap7/redux-sample/src/store/AppState.tsx
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { UserReducer } from './UserReducer';
+import { PostReducer} from "./PostReducer";
 
 // this will be our sum of all the reducers passed
 export const rootReducer = combineReducers({
-    user: UserReducer
+    user: UserReducer,
+    post: PostReducer
 });
 
 export type AppState = ReturnType<typeof rootReducer>;
