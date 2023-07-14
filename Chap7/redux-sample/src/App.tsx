4d65a4a5796da0e9dc06f485bbaaeea0722ee08f
JonEast87 <j.d.east@icloud.com>
2023-07-13 18:57:23 -0700

    wrapped up redux mini project

--- Chap7/redux-sample/src/App.tsx
+++ Chap7/redux-sample/src/App.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
+import './App.css';
 import { useDispatch } from 'react-redux';
 import { USER_TYPE} from "./store/UserReducer";
 import UserDisplay from './store/UserDisplay';
-// import ContextTester from './ContextTester';
-import './App.css';
+import PostDisplay from "./PostDisplay";
+import { POST_TYPE } from './store/PostReducer'
 
 // we will be grabbing the id to check against our JSON API for the matching userid
 function App() {
   const [userid, setUserId] = useState(0);
   const dispatch = useDispatch();
+  const [postid, setPostId] = useState(0);
   const onChangeUserId = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const useridFromInput = e.target.value ? Number(e.target.value) : 0;
     console.log('userid', useridFromInput);
@@ -34,13 +36,47 @@ function App() {
       });
     }
   }
+
+  const onChangePostId = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const postIdFromInput = e.target.value ? Number(e.target.value) : 0;
+    setPostId(postIdFromInput);
+
+    const postResponse = await fetch('https://jsonplaceholder.typicode.com/posts/' + postIdFromInput);
+
+    if (postResponse.ok) {
+      const post = await postResponse.json();
+      console.log('post', post);
+
+      dispatch({
+        type: POST_TYPE,
+        payload: {
+          id: post.id,
+          title: post.title,
+          body: post.body
+        }
+      });
+    }
+  }
+
   return (
     <React.Fragment>
-      <div className="App">
-        <label>user id</label>
-        <input value={userid} onChange={onChangeUserId} />
+      <div style={{ width: "300px" }}>
+        <div className="App">
+          <label>user id</label>
+          <input value={userid} onChange={onChangeUserId} />
+        </div>
+        <UserDisplay />
       </div>
-      <UserDisplay />
+
+      <br />
+
+      <div style={{ width: "300px"}}>
+        <div className="App">
+          <label>post id</label>
+          <input value={postid} onChange={onChangePostId} />
+        </div>
+        <PostDisplay />
+      </div>
     </React.Fragment>
   );
 }
