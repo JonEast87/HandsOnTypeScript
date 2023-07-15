5acbc4e85d51c2d302e5de13a00de5dc64cd1876
JonEast87 <j.d.east@icloud.com>
2023-07-15 09:42:42 -0700

    finished chap7

--- Chap7/try-react-router/src/ScreenC.tsx
+++ Chap7/try-react-router/src/ScreenC.tsx
@@ -1,4 +1,5 @@
 import React, {FC, useEffect} from 'react';
+import { useNavigate, useParams } from "react-router-dom";
 
 interface ScreenCProps {
     message: string;
@@ -8,15 +9,29 @@ interface ScreenCProps {
 
 // changed from object destructing to simply passing props to it to ensure you can access any value
 const ScreenC: FC<ScreenCProps> = (props) => {
-    useEffect(() => {
-        setTimeout(() => {
-           props.history.push('/');
-        }, 3000);
-    });
+    // useEffect(() => {
+
+    //     setTimeout(() => {
+    //        props.history.push('/');
+    //     }, 3000);
+    // });
+
+    const navigate = useNavigate();
+    const { userid } = useParams();
+
+    const onClickGoback = () => {
+        // props.history.goBack();
+        // cannot utilize useHistory hook for this version of React like the book asks for, replaced it with useNavigate
+        navigate(-1);
+    }
+
 
     return (<div>
                 <div>{'Your id is ' + props.match.params.userid}</div>
                 <div>{props.message}</div>
+                <div>
+                    <button onClick={onClickGoback}>Go back</button>
+                </div>
             </div>);
 };
 
