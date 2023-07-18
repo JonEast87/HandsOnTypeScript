c9fed866e5505bf3db1e5fbc5e8c30c98bb0c1f1
JonEast87 <j.d.east@icloud.com>
2023-07-17 20:37:48 -0700

    preapring to work on middleware

--- Chap8/node-server/expressapp.mjs
+++ Chap8/node-server/expressapp.mjs
@@ -2,7 +2,15 @@ import express from 'express';
 
 const app = express();
 
-app.listen({ port: 8000 }, () => {
-    console.log('Express Node server has loaded!');
+app.use((req, res, next) => {
+    console.log('First middleware');
+    next();
 });
 
+app.use((req, res, next) => {
+    res.send('Hello world. I am custom middleware.');
+});
+
+app.listen({ port: 8000 }, () => {
+    console.log('Express Node server has loaded.');
+});
